d70a56670bcd7abafec29aefdb75026a2a4cbcc8
farhadham <farhadham2@gmail.com>
2022-06-10 11:43:40 +0300

    working on h2 again

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./Header.module.scss";
 import BackgroundImage from "../BackgroundImage/BackgroundImage.js";
 import Container from "../Container/Container";
-
-const texts = ["develope websites"];
+import { motion } from "framer-motion";
 
 const Header = ({ color, lines }) => {
-  const [text, setText] = useState("I");
-
-  useEffect(() => {
-    for (let i = 0; i < texts[0].length; i++) {
-      setTimeout(() => {
-        console.log(i);
-        setText("I " + texts[0].slice(0, i + 1));
-      }, 50 + i * 50);
-    }
-  }, []);
-
   return (
     <div className={styles.wrapper} id="header">
       <BackgroundImage />
       <Container lines={lines}>
-        <h2>{text}</h2>
+        <motion.div
+          className={styles.h2}
+          initial={{ width: "auto" }}
+          animate={{ width: "0%" }}
+          transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
+        >
+          <h2>I develope websites</h2>
+          <h2>farhad faraji</h2>
+        </motion.div>
+
         <h1>
           Hello, I’m <strong style={{ color: color }}>Farhad Faraji</strong>,
           Web Developer, Game Designer,
